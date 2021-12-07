9c968026e53c7f969aa42a818d8531961eb2620d
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-07 22:52:24 +0900

    db connect setup func

---
+++ api/models/dbconnect.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbUserName := os.Getenv("DB_ID")
+	dbPassWord := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUserName, dbPassWord, dbHost, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("connected db failed")
+	}
+
+	println("connected on db")
+
+	db.AutoMigrate(&Account{})
+
+	DB = db
+}
