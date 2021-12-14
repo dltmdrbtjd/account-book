8bc806f97b945f33f056a35cc4520f2224e062ae
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-14 23:26:58 +0900

    Fix: Lint Error

--- fe/.eslintrc.js
+++ fe/.eslintrc.js
@@ -3,11 +3,15 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:vue/essential', 'airbnb-base', 'prettier'],
+  extends: ['plugin:vue/essential', 'airbnb-base', 'prettier', 'prettier/vue'],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: 'module',
   },
   plugins: ['vue', 'prettier'],
-  rules: {},
+  rules: {
+    'vue/multi-word-component-names': 0,
+    'import/no-unresolved': 0,
+    'vue/no-multiple-template-root': 0,
+  },
 };
