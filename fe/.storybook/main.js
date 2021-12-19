df6c2b6e9ff216bab1ea5a75b833631b33f46825
dltmdrbtjd <dltmdrbtjd@gmail.com>
2021-12-19 17:07:01 +0900

    storybook scss loader testing

--- fe/.storybook/main.js
+++ fe/.storybook/main.js
@@ -1,11 +1,33 @@
+const path = require('path');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        cssLoaderOptions: {
+          modules: true,
+          localIdentName: '[name]__[local]--[hash:base64:5]',
+        },
+      },
+    },
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
-  "framework": "@storybook/vue3"
-}
+  framework: '@storybook/vue3',
+  webpackFinal: async (config, {configType}) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader',
+        miniCssExtractPlugin.loader,
+      ],
+      include: path.resolve(__dirname, '../'),
+    });
+    return config;
+  },
+};
